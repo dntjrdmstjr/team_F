a6d1a9fc3b71786d58236aa8e7bc218db1a4bdb9
305 <305@DESKTOP-9OTDVF2>
2024-11-13 16:40:34 +0900

    2024/11/13 16:40 빙주현 확인용 코드

--- Gameproject/src/GameStart.java
+++ Gameproject/src/GameStart.java
@@ -3,7 +3,10 @@ public class GameStart {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		test te = new test();
+		te.color = "blue";
+		te.price = 100;
+		te.write("Hello world");
 	}
 
 }
--- Gameproject/src/test.java
+++ Gameproject/src/test.java
@@ -1,4 +1,9 @@
 
 public class test {
-
+	String color;
+	int price;
+	
+	public void write(String msg) {
+		System.out.println(msg);
+	}
 }
