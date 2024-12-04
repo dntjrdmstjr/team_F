47d774682a9cd1814549fd64aeabe75ad787d10a
305 <305@DESKTOP-9OTDVF2>
2024-12-04 16:03:32 +0900

    2024/12/04 16:02 mainframe 만듬

--- Gameproject/src/GameStart.java
+++ Gameproject/src/GameStart.java
@@ -2,11 +2,7 @@
 public class GameStart {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		test te = new test();
-		te.color = "blue";
-		te.price = 100;
-		te.write("Hello world");
+		new MainFrame();
 	}
 
 }
